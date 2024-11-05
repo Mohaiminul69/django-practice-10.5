65393028b2ef6df750fe1af4a95fc3037d74eecb
mohaiminul69 <mohaiminul69@gmail.com>
2024-11-05 23:57:20 +0600

    filters used.

--- module-10.5/sample_project/first_app/views.py
+++ module-10.5/sample_project/first_app/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import datetime
+
 
 # Create your views here.
+def home(request):
+    d = {
+        "author": "rahim",
+        "sentence": "How are you everybody?",
+        "age": 4,
+        "list": ["python", "is", "best"],
+        "birthday": datetime.now(),
+        "emptyValue": "",
+        "courses": [
+            {"id": 1, "name": "Python", "fee": 5000},
+            {"id": 2, "name": "Django", "fee": 6000},
+        ],
+    }
+    return render(request, "first_app/home.html", d)
